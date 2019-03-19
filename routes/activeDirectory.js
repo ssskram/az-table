8ea4053d248f2fc659a96fe57f48d786be05f9d9
ssskram <ssskram@ssskram.local>
2019-03-19 13:52:30 -0400

    removed addEvent endpoint

--- routes/activeDirectory.js
+++ routes/activeDirectory.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 const checkToken = require('../token')
 const fetch = require('node-fetch')
-const azure = require('azure-storage')
-const tableService = azure.createTableService()
 const dt = require('node-json-transform').DataTransform
 const models = require('../models/activeDirectory')
 global.Headers = fetch.Headers
@@ -37,37 +35,6 @@ router.get('/riskEvents',
                 } else return
             }
 
-        } else res.status(403).end()
-    }
-)
-
-// post new event
-router.post('/newEvent',
-    async function (req, res) {
-        const valid = (checkToken(req.token))
-        if (valid == true) {
-            const entGen = azure.TableUtilities.entityGenerator
-            const entity = {
-                PartitionKey: entGen.String(req.body.userName),
-                RowKey: entGen.String(req.body.id),
-                eventTime: req.body.time,
-                userEmail: req.body.userEmail,
-                appName: req.body.appName,
-                ipAddress: req.body.ipAddress,
-                city: req.body.city,
-                state: req.body.state,
-                country: req.body.country,
-                latitude: req.body.latitude,
-                longitude: req.body.longitude
-            }
-            tableService.insertOrReplaceEntity('adEvents', entity, function (error, result, response) {
-                if (!error) {
-                    res.status(200).send()
-                } else {
-                    console.log(error)
-                    res.status(500).send(error)
-                }
-            });
         } else res.status(403).end()
     }
 )
