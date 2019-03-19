b287de9f75212fc6474ce9bebcd2552d32660d1a
ssskram <ssskram@ssskram.local>
2019-03-19 18:09:57 -0400

    toDelete endpoint

--- routes/activeDirectory.js
+++ routes/activeDirectory.js
@@ -5,6 +5,7 @@ const fetch = require('node-fetch')
 const dt = require('node-json-transform').DataTransform
 const azure = require('azure-storage')
 const tableService = azure.createTableService()
+var QueryComparisons = azure.TableUtilities.QueryComparisons
 const models = require('../models/activeDirectory')
 global.Headers = fetch.Headers
 
@@ -40,5 +41,41 @@ router.get('/riskEvents',
         } else res.status(403).end()
     }
 )
+
+// returns events older than 48 hours
+router.get('/toDelete',
+    async function (req, res) {
+        const valid = (checkToken(req.token))
+        if (valid == true) {
+            const events = []
+            const date = new Date
+            await date.setHours(date.getHours() - 48)
+            const query = new azure.TableQuery().where('Timestamp le ?', date)
+            await callAPI(null).then(() => {
+                console.log(events.length)
+                res.status(200).send(events)
+            })
+            async function callAPI(page) {
+                const response = await new Promise(async function (resolve, reject) {
+                    await tableService.queryEntities('adEvents', query, page, async (error, result, response) => {
+                        if (!error) {
+                            resolve(result)
+                        } else {
+                            console.log(error)
+                            res.status(500).send()
+                            reject(error)
+                        }
+                    })
+                })
+                await events.push(...dt(response, models.event).transform())
+                if (response.continuationToken) {
+                    await callAPI(response.continuationToken)
+                } else return
+            }
+
+        } else res.status(403).end()
+    }
+)
+
 
 module.exports = router
