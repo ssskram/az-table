e67d7142c31f2896bf113e6501f0d2c2fd799456
ssskram <ssskram@ssskram.local>
2019-03-27 12:29:33 -0400

    prettify

--- models/activeDirectory.js
+++ models/activeDirectory.js
@@ -1,31 +1,30 @@
-
 const event = {
-    list: 'entries',
-    item: {
-        userName: 'PartitionKey._',
-        id: 'RowKey._',
-        time: 'eventTime._',
-        userEmail: 'userEmail._',
-        appName: 'appName._',
-        ipAddress: 'ipAddress._',
-        city: 'city._',
-        state: 'state._',
-        country: 'country._',
-        latitude: 'latitude._',
-        longitude: 'longitude._'
-    }
-}
+  list: "entries",
+  item: {
+    userName: "PartitionKey._",
+    id: "RowKey._",
+    time: "eventTime._",
+    userEmail: "userEmail._",
+    appName: "appName._",
+    ipAddress: "ipAddress._",
+    city: "city._",
+    state: "state._",
+    country: "country._",
+    latitude: "latitude._",
+    longitude: "longitude._"
+  }
+};
 
 const toDelete = {
-    list: 'entries',
-    item: {
-        PartitionKey: 'PartitionKey',
-        RowKey: 'RowKey',
-        eventTime: 'eventTime'
-    }
-}
+  list: "entries",
+  item: {
+    PartitionKey: "PartitionKey",
+    RowKey: "RowKey",
+    eventTime: "eventTime"
+  }
+};
 
 module.exports = {
-    event,
-    toDelete
-}
+  event,
+  toDelete
+};
--- routes/activeDirectory.js
+++ routes/activeDirectory.js
@@ -1,64 +1,71 @@
-const express = require('express')
-const router = express.Router()
-const dt = require('node-json-transform').DataTransform
-const azure = require('azure-storage')
-const tableService = azure.createTableService()
-const models = require('../models/activeDirectory')
+const express = require("express");
+const router = express.Router();
+const dt = require("node-json-transform").DataTransform;
+const azure = require("azure-storage");
+const tableService = azure.createTableService();
+const models = require("../models/activeDirectory");
 
 // get events generated from outside PA
-router.get('/riskEvents', async (req, res) => {
-    const events = []
-    const query = new azure.TableQuery()
-        .where('state ne ?', 'Pennsylvania')
-    await callAPI(null).then(() => {
-        res.status(200).send(events)
-    })
+router.get("/riskEvents", async (req, res) => {
+  const events = [];
+  const query = new azure.TableQuery().where("state ne ?", "Pennsylvania");
+  await callAPI(null).then(() => {
+    res.status(200).send(events);
+  });
 
-    async function callAPI(page) {
-        const response = await new Promise(async function (resolve, reject) {
-            await tableService.queryEntities('adEvents', query, page, async (error, result, response) => {
-                if (!error) {
-                    resolve(result)
-                } else {
-                    res.status(500).send()
-                    reject(error)
-                }
-            })
-        })
-        await events.push(...dt(response, models.event).transform())
-        if (response.continuationToken) {
-            await callAPI(response.continuationToken)
-        } else return
-    }
-})
+  async function callAPI(page) {
+    const response = await new Promise(async function(resolve, reject) {
+      await tableService.queryEntities(
+        "adEvents",
+        query,
+        page,
+        async (error, result, response) => {
+          if (!error) {
+            resolve(result);
+          } else {
+            res.status(500).send();
+            reject(error);
+          }
+        }
+      );
+    });
+    await events.push(...dt(response, models.event).transform());
+    if (response.continuationToken) {
+      await callAPI(response.continuationToken);
+    } else return;
+  }
+});
 
 // returns events older than 48 hours
-router.get('/toDelete', async (req, res) => {
-    const events = []
-    const date = new Date
-    await date.setHours(date.getHours() - 48)
-    const query = new azure.TableQuery()
-        .where('Timestamp le ?', date)
-    await callAPI(null).then(() => {
-        res.status(200).send(events)
-    })
-    async function callAPI(page) {
-        const response = await new Promise(async function (resolve, reject) {
-            await tableService.queryEntities('adEvents', query, page, async (error, result, response) => {
-                if (!error) {
-                    resolve(result)
-                } else {
-                    res.status(500).send()
-                    reject(error)
-                }
-            })
-        })
-        await events.push(...dt(response, models.toDelete).transform())
-        if (response.continuationToken) {
-            await callAPI(response.continuationToken)
-        } else return
-    }
-})
+router.get("/toDelete", async (req, res) => {
+  const events = [];
+  const date = new Date();
+  await date.setHours(date.getHours() - 48);
+  const query = new azure.TableQuery().where("Timestamp le ?", date);
+  await callAPI(null).then(() => {
+    res.status(200).send(events);
+  });
+  async function callAPI(page) {
+    const response = await new Promise(async function(resolve, reject) {
+      await tableService.queryEntities(
+        "adEvents",
+        query,
+        page,
+        async (error, result, response) => {
+          if (!error) {
+            resolve(result);
+          } else {
+            res.status(500).send();
+            reject(error);
+          }
+        }
+      );
+    });
+    await events.push(...dt(response, models.toDelete).transform());
+    if (response.continuationToken) {
+      await callAPI(response.continuationToken);
+    } else return;
+  }
+});
 
-
-module.exports = router
+module.exports = router;
